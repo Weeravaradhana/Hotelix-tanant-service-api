84ecfc8bff4d1b11b0669295e1a6cd643c4b2b22
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-31 12:04:04 +0530

    Initial commit

---
+++ src/main/java/com/hotelix/hotel_system/TanantServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotelix.hotel_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanantServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanantServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelix/hotel_system/TanantServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelix.hotel_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanantServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
