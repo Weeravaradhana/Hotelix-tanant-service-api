daa8261d8aa11ccc4690037dd2124510b8d6ab91
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-31 13:21:04 +0530

    feat(tenant-service): modify main class

--- src/main/java/com/hotelix/hotel_system/TenantServiceApiApplication.java
+++ src/main/java/com/hotelix/hotel_system/TenantServiceApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TanantServiceApiApplication {
+public class TenantServiceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TanantServiceApiApplication.class, args);
+		SpringApplication.run(TenantServiceApiApplication.class, args);
 	}
 
 }
