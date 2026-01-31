825bdc4d7dad66b323acac57b58f170363181809
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-31 14:59:42 +0530

    feat(tenant-service): add TenantAlreadyExistsException

---
+++ src/main/java/com/hotelix/hotel_system/exception/TenantAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.hotelix.hotel_system.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class TenantAlreadyExistsException extends RuntimeException {
+    public TenantAlreadyExistsException(String message) {
+        super(message);
+    }
+}
